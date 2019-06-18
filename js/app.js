a6549337d3342966c516f33f060f29866999e857
FabianB14 <32110808+FabianB14@users.noreply.github.com>
2019-06-18 02:16:57 -0700

    I still have a few kinks to work out

--- js/app.js
+++ js/app.js
@@ -19,4 +19,124 @@ I calculate the percentage of the clicks per image:
 Using a calculator function I will caluclate the percentage from clicks of 25
 
 I will need a constructor for the images.:
-I will implement this constructor to have an object associated with each image. The properties will be name, filepath, number of times the image has been shown and the numeber of times it has been clicked  */
+I will implement this constructor to have an object associated with each image. The properties will be name, filepath, number of times the image has been shown and the numeber of times it has been clicked
+
+The user will need instructions
+
+Need to size images correctly
+
+*/
+
+
+//Gloabal Variables
+
+var allImageTag = document.getElementById('all_images');
+var leftImageTag = document.getElementById('left_img');
+var middleImageTag = document.getElementById('middle_img');
+var rightImageTag = document.getElementById('right_img');
+//Count for all image clicks
+var globalClickCount = 0;
+//These Variables will represent the images on the page at the moment
+
+var leftImageOnPage;
+var middleImageOnPage;
+var rightImageOnPage;
+
+//This is where the image object constructor will be
+var ImageObjet = function(name,imageSrc){
+  this.name = name;
+  this.url = imageSrc;
+  this.clicks = 0;
+  this.timeShown = 0;
+
+  ImageObjet.allImages.push(this);
+};
+
+ImageObjet.allImages = [];
+
+var renderImages = function(leftIndex,middleIndex,rightIndex){
+  leftImageTag.src = ImageObjet.allImages[leftIndex].url;
+  middleImageTag.src = ImageObjet.allImages[middleIndex].url;
+  rightImageTag.src = ImageObjet.allImages[rightIndex].url;
+};
+
+var clickNewImages = function(){
+  var leftIndex = Math.round(Math.random()*ImageObjet.allImages.length);
+  do{
+    var middleIndex = Math.round(Math.random()*ImageObjet.allImages.length);
+    var rightIndex = Math.round(Math.random()*ImageObjet.allImages.length);
+  }while(rightIndex === middleIndex || rightIndex === leftIndex || leftIndex === middleIndex);
+
+  leftImageOnPage = ImageObjet.allImages[leftIndex];
+  middleImageOnPage = ImageObjet.allImages[middleIndex];
+  rightImageOnPage = ImageObjet.allImages[rightIndex];
+
+  renderImages(leftIndex,middleIndex,rightIndex);
+};
+
+var handleClickOnImages = function(event){
+  if(globalClickCount < 26){
+    var imageClicked = event.target;
+    var id = imageClicked.id;
+
+    if(id === 'left_img' || id === 'middle_img' || id === 'right_img'){
+
+      if(id === 'left_img'){
+        leftImageOnPage.clicks++;
+      }
+      if(id === 'middle_img'){
+        middleImageOnPage.clicks++;
+      }
+      if(id === 'right_img'){
+        rightImageOnPage.clicks++;
+      }
+      leftImageOnPage.timeShown++;
+      middleImageOnPage.timeShown++;
+      rightImageOnPage.timeShown++;
+
+      clickNewImages();
+    }
+
+  }
+  globalClickCount++;
+  console.log(globalClickCount);
+  if(globalClickCount === 25){
+    allImageTag.removeEventListener('click', handleClickOnImages);
+  }
+};
+
+
+allImageTag.addEventListener('click', handleClickOnImages);
+
+new ImageObjet('bag', './img/bag.jpg');
+new ImageObjet('banana', './img/banana.jpg');
+new ImageObjet('bathroom', './img/bathroom.jpg');
+new ImageObjet('boots', './img/boots.jpg');
+new ImageObjet('breakfast', './img/breakfast.jpg');
+new ImageObjet('pet-sweep', './img/pet-sweep.jpg');
+new ImageObjet('bubblegum', './img/bubblegum.jpg');
+new ImageObjet('chair', './img/chair.jpg');
+new ImageObjet('cthulhu', './img/cthulhu.jpg');
+new ImageObjet('dog-duck', './img/dog-duck.jpg');
+new ImageObjet('dragon', './img/dragon.jpg');
+new ImageObjet('pet', './img/pen.jpg');
+new ImageObjet('scissors', './img/scissors.jpg');
+new ImageObjet('shark', './img/shark.jpg');
+new ImageObjet('sweep', './img/sweep.png');
+new ImageObjet('tauntaun', './img/tauntaun.jpg');
+new ImageObjet('usb', './img/usb.gif');
+new ImageObjet('water', './img/water-can.jpg');
+new ImageObjet('wine', './img/wine-glass.jpg');
+
+console.log(ImageObjet.allImages);
+
+leftImageOnPage = ImageObjet.allImages[5];
+middleImageOnPage = ImageObjet.allImages[17];
+rightImageOnPage = ImageObjet.allImages[0];
+console.log(globalClickCount);
+
+
+clickNewImages();
+
+
+
