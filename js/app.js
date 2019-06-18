621d01674754ff1ae6a23be4d12e519ec6f8d181
FabianB14 <32110808+FabianB14@users.noreply.github.com>
2019-06-18 00:01:19 -0700

    Added some html and comments for JS

---
+++ js/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/*  Thing needed:
+I will need an event listner to listen for clicks:
+I will have an event listener for 3 images at a time
+
+I will need an array of images:
+after constructing images I will have them pushed to an array
+
+I will need random image generator function:
+The random image function will take in the array of images and randomly send 3 images at a time to each image div
+
+I will need to keep count of the number of clicks (maximum 25):
+I will have the event listener function keep count of of the number of clicks and once maximum clicks are reached the event listener will turn off
+
+I will keep duplicates from poping up after a click:
+I will save the 3 previous images on the into a mini image array, if any of the images were up then the function will run again untill new images are present. "Still thinking more about how to accomplish this feat. I will save for last"
+I calculate the percentage of the clicks per image:
+Using a calculator function I will caluclate the percentage from clicks of 25
+
+I will need a constructor for the images.:
+I will implement this constructor to have an object associated with each image. The properties will be name, filepath, number of times the image has been shown and the numeber of times it has been clicked  */
