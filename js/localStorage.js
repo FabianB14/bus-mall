77ebcb85faeed261075f6eed47e479464307606f
FabianB14 <32110808+FabianB14@users.noreply.github.com>
2019-06-20 06:52:12 -0700

    I have Local Storage working.....I just have few more tweaks

---
+++ js/localStorage.js
@@ -0,0 +1,33 @@
+'use strict';
+
+
+// var name = 'Fabian';
+
+// var fabian = {
+//   name:'Fabe',
+//   age:'Too Old',
+//   height:'10000'
+// };
+
+// var arrayLocal = [2,4,5,3,6,7];
+
+// var imageObjectString = JSON.stringify(ImageObject.allImages);
+
+// localStorage.setItem('Myname', name);
+
+// localStorage.setItem('stringOfFabian', imageObjectString);
+
+
+
+// localStorage.setItem('arrayLocal',JSON.stringify(arrayLocal));
+
+// var unstringFabian = JSON.parse(imageObjectString);
+
+// // localStorage.setItem('Click Count', globalClickCount);
+
+// console.log(unstringFabian);
+
+
+
+
+
